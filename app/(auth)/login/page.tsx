c4989ade52e901c0878c3e842295628b69b55c65
Sorawit <sorawit12535@gmail.com>
2024-09-08 23:18:46 +0700

    feat: login page and navigation bar

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Login | Drawn",
+  description: "Collaboration Web App",
+};
+
+export default function LoginPage() {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader className="flex flex-col justify-center items-center">
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            placeholder="hi@example.com"
+            required
+          />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" required />
+        </div>
+        <div className="w-full flex justify-center">
+          <small>
+            Does not have an account? <Link href="/register">Register</Link>
+          </small>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">Sign in</Button>
+      </CardFooter>
+    </Card>
+  );
+}
